4d60d5fcd466307cda73b909f8b91073d6378c57
Marshall Powell <marshallpowell13@gmail.com>
2024-06-03 17:57:18 +0100

    Author model complete

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+
+const mongoDB = process.env.mongoDBURI;
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(mongodb);
+}
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
