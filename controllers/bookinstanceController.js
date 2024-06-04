d63a2497d6a2183d1b007fd8d11b4d3fb558062b
Marshall Powell <marshallpowell13@gmail.com>
2024-06-04 10:21:08 +0100

    Refactor authorController.js to implement missing routes and functions
    - Implemented `author_list` route and handler
    - Implemented `author_detail` route and handler
    - Implemented `author_create_get` route and handler
    - Implemented `author_create_post` route and handler
    - Implemented `author_delete_get` route and handler
    - Implemented `author_delete_post` route and handler
    - Implemented `author_update_get` route and handler
    - Implemented `author_update_post` route and handler
    
    Also, added virtual properties to the Author model for better data representation.

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -2,7 +2,13 @@ const BookInstance = require('../models/bookinstance');
 const asyncHandler = require('express-async-handler');
 
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: BookInstance list');
+    
+    const bookinstances = await BookInstance.find().populate('book').exec();
+
+    res.render('bookinstance_list', {
+        title: 'Book Instance List',
+        bookinstance_list: bookinstances
+    })
 })
 
 exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
